47dfd4882ab1167815ba33eaa3419a8b07b2af56
tamzid2001 <tamzid257@gmail.com>
2015-07-27 14:27:42 -0400

    img of the day gen

--- pic_engine.py
+++ pic_engine.py
@@ -7,30 +7,22 @@ def date():
 	b = str(random.randint(1,12))
 	if len(b) == 1:
 		b = "0" + b
-	c = str(random.randint(1,30))
+	c = str(random.randint(1,29))
 	if len(c) == 1:
 		c = "0" + c
 	date = a + "-" + b +"-" + c
 	return date
 
-
-
-print a
-print b
-print c
+u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?date="+date()+"&concept_tags=True&api_key=DEMO_KEY")
+xtextx = u.read()
+text = json.loads(xtextx)
 
 def get_img():
-	u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?date="+date()+"&concept_tags=True&api_key=DEMO_KEY")
-	xtextx = u.read()
-	text = json.loads(xtextx)
 	nasa_img = text['url']
 	return nasa_img
 
 	
-#def get_title():
-#	q = urllib2.urlopen("https://api.nasa.gov/planetary/apod?date="+a+"-"+b+"-"+c+"06&concept_tags=True&api_key=DEMO_KEY")
-#	xxtextxx = u.read()
-#	xxxtextxxx = json.loads(xxtextxx)
-#	_title = xxxtextxxx['title']
-#	return _title
+def get_title():
+	_title = text['title']
+	return _title
 
--- pic_flask.py
+++ pic_flask.py
@@ -6,8 +6,9 @@ pic_flask = Flask(__name__)
 @pic_flask.route('/')
 def pic():
 	i = pic_engine.get_img()
+	t = pic_engine.get_title()
 	#i = "http://apod.nasa.gov/apod/image/1507/MWAurora_hang_960.jpg"
-	return render_template("pic_img.html", x = i)
+	return render_template("pic_img.html", x = i, k = t)
 
 if __name__ == "__main__":
 	pic_flask.debug = True
