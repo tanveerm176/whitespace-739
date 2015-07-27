d81cc091e5c9b1418ac5e468b326178ab94b4e47
tamzid2001 <tamzid257@gmail.com>
2015-07-27 13:14:17 -0400

    img of the day gen

---
+++ pic_flask.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import pic_engine
+
+pic_flask = Flask(__name__)
+
+@pic_flask.route('/')
+def pic():
+	i = pic_engine.get_img()
+	#i = "http://apod.nasa.gov/apod/image/1507/MWAurora_hang_960.jpg"
+	return render_template("pic_img.html", x = i)
+
+if __name__ == "__main__":
+	pic_flask.debug = True
+	pic_flask.run()
