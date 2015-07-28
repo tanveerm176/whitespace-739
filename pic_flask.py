a489b61e7222fa274a57d49fdef0e97200fae7c6
Chris Mallalieu <chrismallalieu@yahoo.com>
2015-07-28 14:07:12 -0400

    New File

--- pic_flask.py
+++ pic_flask.py
@@ -2,14 +2,13 @@ from flask import Flask, render_template
 import pic_engine
 
 pic_flask = Flask(__name__)
-
 @pic_flask.route('/')
 def pic():
-	i = pic_engine.get_img()
-	t = pic_engine.get_title()
-	#i = "http://apod.nasa.gov/apod/image/1507/MWAurora_hang_960.jpg"
-	return render_template("pic_img.html", x = i, k = t)
+        i = pic_engine.get_img()
+        t = pic_engine.get_title()
+        f = pic_engine.get_expl()
+        return render_template("finalHome.html", x = i, k = t, h = f)
 
 if __name__ == "__main__":
-	pic_flask.debug = True
-	pic_flask.run()
+        pic_flask.debug = True
+        pic_flask.run()
