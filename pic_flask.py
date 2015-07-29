df79e4b7f7f8d76492ae077322471c4f32dcfe30
tanveerm176 <tanveerm176@gmail.com>
2015-07-29 10:42:31 -0400

    changed variables in pic_flask

--- pic_flask.py
+++ pic_flask.py
@@ -4,9 +4,9 @@ import pic_engine
 pic_flask = Flask(__name__)
 @pic_flask.route('/')
 def pic():
-        i = pic_engine.get_img()
-        t = pic_engine.get_title()
-        f = pic_engine.get_expl()
+        i = pic_engine.get_img()[0]
+        t = pic_engine.get_img()[1]
+        f = pic_engine.get_img()[2]
         return render_template("finalHome.html", x = i, k = t, h = f)
 
 if __name__ == "__main__":
